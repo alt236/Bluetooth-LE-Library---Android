851c0ccf724151e862f391e92423a1229c16c738
Alexandros Schillings <aschillings@gmail.com>
2014-03-10 20:04:35 +0000

    Started work on the iBeacon Manufacturer Data parsing

--- src/uk/co/alt236/btlescan/containers/ManufacturerDataIBeacon.java
+++ src/uk/co/alt236/btlescan/containers/ManufacturerDataIBeacon.java
@@ -1,6 +1,33 @@
 package uk.co.alt236.btlescan.containers;
 
 public final class ManufacturerDataIBeacon {
+// 0	FF # Manufacturer specific data AD type
+// 1	4C # Byte 1 of Company identifier code
+// 2	00 # Byte 0 of Company identifier code (0x004C == Apple)
+// 3	02 # Byte 0 of iBeacon advertisement indicator
+// 4	15 # Byte 1 of iBeacon advertisement indicator
+// 5	e2 #
+// 6	c5 ##
+// 7	6d ###
+// 8	b5 ####
+// 9	df #####
+// 10	fb ######
+// 11	48 #######
+// 12	d2 ######## iBeacon proximity uuid
+// 13	b0 ########
+// 14	60 #######
+// 15	d0 ######
+// 16	f5 #####
+// 17	a7 ####
+// 18	10 ###
+// 19	96 ##
+// 20	e0 #
+// 21	00
+// 22	00 # major
+// 23	00
+// 24	00 # minor
+// 25	c5 # The 2's complement of the calibrated Tx Power
+
 	private final byte[] mData;
 	private final int mTxPower;
 
